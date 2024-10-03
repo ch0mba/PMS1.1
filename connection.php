b522a9a3b2262b0a1bc14140e3f3887cbae88572
ch0mba <chombaerickdickson@gmail.com>
2024-10-03 09:23:42 +0300

    first commit

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost:3307"; // Replace with your server name
+$username = "root"; // Replace with your username
+$password = ""; // Replace with your password
+$database = "productiondb"; // Replace with your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
