f964c866b59794f8405452f3b5d1304c2586aaa7
ch0mba <chombaerickdickson@gmail.com>
2024-11-08 17:03:16 +0300

    Bootstap added

--- pages/supervisor_setup.php
+++ pages/supervisor_setup.php
@@ -12,11 +12,10 @@
     include '../api/connection.php';
     ?>
 <body>
-    <h1>Supervisor Setup</h1>
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="">Navbar</a>
+            <a class="navbar-brand" href=""><h1>SUPERVISOR SETUP</h1></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -24,41 +23,45 @@
            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="../pages/dashboard.php">Home</a>
                         </li>
 
-                       <li class="nav-item"a class="nav-link" href="#">Link</a></li>
-                       <li class="nav-item dropdown">
-                       <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> 
-                            Dropdown
-                       </a>
-                            <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                             </ul>
-                              </li>
                         <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                            <a class="nav-link active" aria-current="page" href="../pages/schedule.php">Job Tracking</a>
                         </li>
+
+                       <li class="nav-item dropdown">
+                           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> 
+                            Setup
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                            <li class="nav-item">
+                               <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                             </li>
                             </ul>
                             <form class="d-flex" role="search">
                                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                                 <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-    </div>
-  </div>
+                             </form>
+                </div>
+       </div>
 </nav>
 
 
     <!-- From for adding supervisors -->
-    <div clas="form" action=""/>
+    <div clas="form" action="POST">
         <h3>Add Supervisor</h3>
         <label for="supervisor_id">
             <i class="bi bi-person"></i>
            Supervisor ID:
         </label>
+
         <input type="text" name="supervisor_id" id ="supervisor_id" placeholder="Danco Number" required>
 
         <label for="first_name">First Name:</label>
